35e304e63180b867be98e30e5255502dc8d97c3f
evanadwyer <evanadwyer@gmail.com>
2024-05-14 17:24:05 -0700

    7.3 Setting up permission handling

--- run/presentation/src/main/java/com/evanadwyer/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/evanadwyer/run/presentation/active_run/ActiveRunViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
 class ActiveRunViewModel(
@@ -17,9 +18,37 @@ class ActiveRunViewModel(
     private val eventChannel = Channel<ActiveRunEvent>()
     val events = eventChannel.receiveAsFlow()
 
+    private val _hasLocationPermission = MutableStateFlow(false)
 
     fun onAction(action: ActiveRunAction) {
         when (action) {
+            ActiveRunAction.OnFinishRunClick -> {
+
+            }
+            ActiveRunAction.OnResumeRunClick -> {
+
+            }
+            ActiveRunAction.OnToggleRunClick -> {
+
+            }
+            is ActiveRunAction.SubmitLocationPermissionInfo -> {
+                _hasLocationPermission.value = action.acceptedLocationPermission
+                state = state.copy(
+                    showLocationRationale = action.showLocationRationale
+                )
+            }
+            is ActiveRunAction.SubmitNotificationPermissionInfo -> {
+                state = state.copy(
+                    showNotificationRationale = action.showNotificationRationale
+                )
+            }
+
+            ActiveRunAction.DismissRationaleDialog -> {
+                state = state.copy(
+                    showNotificationRationale = false,
+                    showLocationRationale = false
+                )
+            }
 
             else -> Unit
         }

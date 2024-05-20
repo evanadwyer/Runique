4e72c3422d3c5dcc706ce92a7d401b9400b20231
evanadwyer <evanadwyer@gmail.com>
2024-05-20 14:24:58 -0700

    Wear.3.5 Setting up the Wear OS permission handling

--- wear/run/presentation/src/main/java/com/evanadwyer/wear/run/presentation/TrackerViewModel.kt
+++ wear/run/presentation/src/main/java/com/evanadwyer/wear/run/presentation/TrackerViewModel.kt
@@ -4,15 +4,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.evanadwyer.wear.run.domain.ExerciseTracker
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class TrackerViewModel(
-
+    private val exerciseTracker: ExerciseTracker,
 ) : ViewModel() {
 
     var state by mutableStateOf(TrackerState())
         private set
+
+    private val hasBodySensorPermission = MutableStateFlow(false)
 
     private val eventChannel = Channel<TrackerEvent>()
     val events = eventChannel.receiveAsFlow()
@@ -20,8 +26,21 @@ class TrackerViewModel(
 
     fun onAction(action: TrackerAction) {
         when (action) {
+            is TrackerAction.OnBodySensorPermissionResult -> {
+                hasBodySensorPermission.value = action.isGranted
+                if (action.isGranted) {
+                    viewModelScope.launch {
+                        val isHeartRateTrackingSupported =
+                            exerciseTracker.isHeartRateTrackingSupported()
+                        state = state.copy(
+                            canTrackHeartRate = isHeartRateTrackingSupported
+                        )
+                    }
+                }
+            }
 
-            else -> Unit
+            TrackerAction.OnFinishRunClick -> Unit
+            TrackerAction.OnToggleRunClick -> Unit
         }
     }
 }
